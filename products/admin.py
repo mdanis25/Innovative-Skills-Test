d183f078c7151d79638d00479e672811381b8edc
Md Anis <rjanis2083@gmail.com>
2025-04-14 06:58:31 +0600

    add products app

---
+++ products/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'price', 'stock', 'created_at', 'updated_at']
+    search_fields = ('name', 'derescription')
+    ordering = ('-created_at',)
+    list_filter = ('created_at', 'updated_at')
+    
---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model): 
+    name = models.CharField(max_length=255) 
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    stock = models.PositiveIntegerField()  
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True) 
+   
+    class Meta: 
+        unique_together = ('name', 'price', 'description')
+        ordering  = ['-created_at']
+    
+    def __str__(self): 
+        return self.name 
+    
